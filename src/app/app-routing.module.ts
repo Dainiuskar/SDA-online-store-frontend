eaaa8e4a707751eddd303b44b3a40224adb2fb0c
Augis08 <73080958+Augis08@users.noreply.github.com>
2021-06-10 15:32:00 +0300

    User registration form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UserAccountFormComponent} from "./user-account/user-account-form/user-account-form.component";
 
 const routes: Routes = [
+    {path: 'addUser', component: UserAccountFormComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {UserAccountFormComponent} from "./user-account/user-account-form/user-account-form.component";
+import {UserService} from "./services/user-service/user.service";
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        UserAccountFormComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        NgbModule
+        NgbModule,
+        FormsModule,
+        HttpClientModule
     ],
-    providers: [],
+    providers: [UserService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
