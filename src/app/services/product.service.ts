4508ca2a1a4af0e571ae1e56219b2469a5dc6980
Augis08 <73080958+Augis08@users.noreply.github.com>
2021-06-12 14:12:23 +0300

    Products list (not final)

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {Product} from "../model/product";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  allProductsUrl = '/server/products/'
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<any> {
+    return this.http.get(this.allProductsUrl)
+  }
+
+  addToCart(product: Product): Observable<any> {
+    return this.http.get(this.allProductsUrl + product.id)
+
+  }
+}
